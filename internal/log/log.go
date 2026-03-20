b4be925a248b510fa45081d92835da942814562b
scalaview <chailink100@gmail.com>
2026-03-20 14:53:52 +0800

    feat: add retrying llm client

---
+++ internal/log/log.go
@@ -0,0 +1,44 @@
+package log
+
+import (
+	"context"
+	"log/slog"
+	"os"
+)
+
+type Logger interface {
+	Debug(msg string, fields ...any)
+	Info(msg string, fields ...any)
+	Warn(msg string, fields ...any)
+	Error(msg string, fields ...any)
+}
+
+type slogLogger struct {
+	inner *slog.Logger
+}
+
+func New(verbose bool) Logger {
+	level := slog.LevelInfo
+	if verbose {
+		level = slog.LevelDebug
+	}
+
+	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: level})
+	return slogLogger{inner: slog.New(handler)}
+}
+
+func (l slogLogger) Debug(msg string, fields ...any) {
+	l.inner.DebugContext(context.Background(), msg, fields...)
+}
+
+func (l slogLogger) Info(msg string, fields ...any) {
+	l.inner.InfoContext(context.Background(), msg, fields...)
+}
+
+func (l slogLogger) Warn(msg string, fields ...any) {
+	l.inner.WarnContext(context.Background(), msg, fields...)
+}
+
+func (l slogLogger) Error(msg string, fields ...any) {
+	l.inner.ErrorContext(context.Background(), msg, fields...)
+}
