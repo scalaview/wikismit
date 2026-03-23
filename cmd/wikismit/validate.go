dc3daad731713151684841844bda93c30ab91f5a
scalaview <chailink100@gmail.com>
2026-03-23 14:32:16 +0800

    feat: add docs validator

--- cmd/wikismit/validate.go
+++ cmd/wikismit/validate.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/scalaview/wikismit/internal/composer"
 	configpkg "github.com/scalaview/wikismit/internal/config"
+	"github.com/scalaview/wikismit/pkg/store"
 	"github.com/spf13/cobra"
 )
 
 func newValidateCmd() *cobra.Command {
-	return newStubCmd("validate", "Validate generated docs", func(cmd *cobra.Command, cfg *configpkg.Config) error {
-		_ = cfg
-		return writeCommandOutput(cmd, "not implemented\n")
-	})
+	return &cobra.Command{
+		Use:   "validate",
+		Short: "Validate generated docs",
+		RunE: runWithConfig(func(cmd *cobra.Command, cfg *configpkg.Config) error {
+			report, err := composer.ValidateDocs(cfg.OutputDir)
+			if err != nil {
+				return err
+			}
+			if err := store.WriteValidationReport(cfg.ArtifactsDir, report); err != nil {
+				return err
+			}
+			return writeCommandOutput(cmd, fmt.Sprintf("Validation complete: %d broken links found in %d files\n", len(report.BrokenLinks), report.TotalFiles))
+		}),
+	}
 }
